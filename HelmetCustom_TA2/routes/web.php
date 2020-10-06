5935c14b9054ec04faff7c7881155d20ee7dad11
lkmnaji <setyoaji.lukman@student.uns.ac.id>
2020-10-06 17:49:30 +0700

    ajiupdate3

--- HelmetCustom_TA2/routes/web.php
+++ HelmetCustom_TA2/routes/web.php
@@ -13,42 +13,42 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get('tesadmin', function () {
-    return view('HelmetCustom.MasterAdmin');
-});
-Route::get('tesdashboardadmin', function () {
-    return view('HelmetCustom.content.Admin.DashboardAdmin');
-});
+// Route::get('tesadmin', function () {
+//     return view('HelmetCustom.MasterAdmin');
+// });
+// Route::get('tesdashboardadmin', function () {
+//     return view('HelmetCustom.content.Admin.DashboardAdmin');
+// });
 
-Route::get('tescustomer', function () {
-    return view('HelmetCustom.content.Customer.customer');
-});
+// Route::get('tescustomer', function () {
+//     return view('HelmetCustom.content.Customer.customer');
+// });
 
-Auth::routes();
-
-
-Route::get('/customer/home', 'Auth\CustomerLoginController@index')->middleware('auth:customer');
-Route::get('/customer/login', 'Auth\CustomerLoginController@showLoginForm')->name('customer.loginform');
-Route::get('/customer/register', 'Auth\CustomerLoginController@showRegisterForm')->name('customer.registerform');
-Route::post('/customer/login', 'Auth\CustomerLoginController@Login')->name('customer.login');
-Route::post('/customer/register', 'Auth\CustomerLoginController@register')->name('customer.register');
-Route::get('/customer/logout', 'Auth\CustomerLoginController@logout')->name('customer.logout');
+// Auth::routes();
 
 
-Route::get('/home', 'HomeController@index')->name('home');
+// Route::get('/customer/home', 'Auth\CustomerLoginController@index')->middleware('auth:customer');
+// Route::get('/customer/login', 'Auth\CustomerLoginController@showLoginForm')->name('customer.loginform');
+// Route::get('/customer/register', 'Auth\CustomerLoginController@showRegisterForm')->name('customer.registerform');
+// Route::post('/customer/login', 'Auth\CustomerLoginController@Login')->name('customer.login');
+// Route::post('/customer/register', 'Auth\CustomerLoginController@register')->name('customer.register');
+// Route::get('/customer/logout', 'Auth\CustomerLoginController@logout')->name('customer.logout');
 
-Route::prefix('AdminHelmetCustom')->group(function(){
-    Route::resource('warna','HelmetCustom\Admin\WarnaController');
-    Route::resource('metodetransaksi','HelmetCustom\Admin\MetodeTransaksiController');
-    Route::resource('produk','HelmetCustom\Admin\ProdukController');
-    Route::resource('status', 'HelmetCustom\Admin\StatusProdukController');
-    Route::resource('order', 'HelmetCustom\Customer\OrderController');
-});
 
-Route::prefix('CustomerHelmetCustom')->group(function(){
-    Route::resource('CustomerData','HelmetCustom\Customer\CustomerDataController');
-});
+// Route::get('/home', 'HomeController@index')->name('home');
+
+// Route::prefix('AdminHelmetCustom')->group(function(){
+//     Route::resource('warna','HelmetCustom\Admin\WarnaController');
+//     Route::resource('metodetransaksi','HelmetCustom\Admin\MetodeTransaksiController');
+//     Route::resource('produk','HelmetCustom\Admin\ProdukController');
+//     Route::resource('status', 'HelmetCustom\Admin\StatusProdukController');
+//     Route::resource('order', 'HelmetCustom\Customer\OrderController');
+// });
+
+// Route::prefix('CustomerHelmetCustom')->group(function(){
+//     Route::resource('CustomerData','HelmetCustom\Customer\CustomerDataController');
+// });
